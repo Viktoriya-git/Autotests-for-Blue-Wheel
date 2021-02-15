3ca3fc8fdbab9cf1516e9bbc43261f9acfc06651
Viktoriya-git <viktoriya.pogorelova@white-test.com>
2021-02-15 16:20:57 +0200

    UPD 16:20 15.02.2021

---
+++ Blue Wheel/page-object/menu.js
@@ -0,0 +1,18 @@
+module.exports = {
+  menu: 'div[class="jet-mobile-menu__before-template"]',
+  backMenuBtn: 'div[aria-label="Back to Prev Items"]',
+  closeMenuBtn: 'div[aria-label="Close Menu"]',
+  closeMailBtn: 'svg[id="a3764f6b-d60e-4c82-b635-15a2a285578b"]',
+  amazonAdv: 'div[aria-label="Amazon Advertising"]',
+  learnMoreAmazonAdv: 'a[class="raven-button raven-button-link"]',
+  digitalMrc: 'div[aria-label="Digital Marketing"]',
+  learnMoreDigMrc: 'a[class="raven-button raven-button-link"]',
+  digitalAdv: 'div[aria-label="Digital Advertising"]',
+  learnMoreDigAdv: 'a[class="raven-button raven-button-link"]',
+  contentCr: 'a[href="https://bluewheelmedia.com/content-creation/"]',
+  amazonAcd: 'a[href="https://bluewheelmedia.com/amazon-advertising-academy/"]',
+  caseStudies: 'div[aria-label="Case Studies"] a[href$="/our-work/"]',
+  newsPress: 'div[aria-label="News &#038; Press"] a[href="https://bluewheelmedia.com/news-press/"]',
+  careers: 'div[aria-label="Careers"] a[href="https://bluewheelmedia.com/careers/"]',
+  contacts: 'div[aria-label="Contact"] a[href="https://bluewheelmedia.com/contact/"]'
+}
